274e8dfe5bd3bb72d675423d38c18f37ea2f12cd
Korek302 <jacek_blady@interia.pl>
2017-10-25 04:06:04 +0200

    25.10

--- lista2.sql
+++ lista2.sql
@@ -6,5 +6,15 @@ WHERE (TEREN = 'POLE' OR TEREN = 'CALOSC')
     
 --zad18
 SELECT K1.IMIE, K1.W_STADKU_OD "POLUJE OD"
-FROM KOCURY K1 JOIN KOCURY K2 ON K1.PSEUDO = K2.PSEUDO 
-WHERE K2.IMIE = 'JACEK' AND K1.W_STADKU_OD < K2.W_STADKU_OD;
+FROM KOCURY K1 JOIN KOCURY K2 ON K1.W_STADKU_OD < K2.W_STADKU_OD
+WHERE K2.IMIE = 'JACEK'
+ORDER BY K1.W_STADKU_OD DESC;
+
+--zad19a
+select k1.imie "Imie", k1.funkcja "Funkcja", NVL(k2.imie, ' ') "Szef 1", NVL(k3.imie, ' ') "Szef 2", NVL(k4.imie, ' ') "Szef 3"
+from ((kocury k1 join kocury k2 on k1.szef = k2.pseudo) left join kocury k3 on k2.szef = k3.pseudo) left join kocury k4 on k3.szef = k4.pseudo
+where k1.funkcja = 'MILUSIA' or k1.funkcja = 'KOT';
+
+--zad19b
+
+--zad19c
