6fc0bc4627c85d4fafc0a924c27043fd5e6f9c42
Korek302 <jacek_blady@interia.pl>
2017-12-20 12:39:23 +0100

    lista4_begin

---
+++ lista4.sql
@@ -0,0 +1,27 @@
+--zad47
+create or replace type Kocury as object
+(imie varchar2(15),
+plec varchar2(1),
+pseudo varchar2(15),
+szef varchar2(15),
+w_stadku_od date default sysdate,
+przydzial_myszy number(3),
+myszy_extra number(3)
+);
+
+create or replace type Elita under Kocury
+(sluga Plebs,
+member function getImie return varchar2,
+pragma restrict_references())
+final;
+
+
+create or replace type Plebs under Kocury
+(
+)
+
+create or replace type Konto_type as object
+(data_wprowadzenia date,
+data_usuniecia date,
+wlasciciel Elita
+)
